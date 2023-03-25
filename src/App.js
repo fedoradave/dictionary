9955a09acabfd11918393323953ec99e6372a632
David Tracey <dwtracey@gmail.com>
2023-03-25 12:58:15 -0400

    add title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Provider } from './contexts/App';
 import Form from './components/Form';
 import Results from './components/Results';
+import Title from './components/Title';
 import Definition from './components/Definition';
 const App = (props) => {
   return (
     <Provider {...props}>
+      <Title />
       <Form />
       <Results />
       <Definition />
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <header>
+      <h1>Squabble<small> Band&#169; Crossword Dictionary</small></h1>
+      <button type="button">&#9776;</button>
+    </header>
+  );
+};
+export default Title;
