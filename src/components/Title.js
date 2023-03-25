aacc1a74794a67e96bb285f70a1976bd7e0b61dd
David Tracey <dwtracey@gmail.com>
2023-03-25 15:00:48 -0400

    title style

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
     <header>
-      <h1>Squabble<small> Band&#169; Crossword Dictionary</small></h1>
+      <h1><strong>SQUABBLE</strong><small> Band&#169; Crossword Dictionary</small></h1>
       <button type="button">&#9776;</button>
     </header>
   );
