a47217228c30905aa19086cca267da60c2fab245
David <dwtracey@gmail.com>
2023-04-24 12:46:35 -0400

    encode decode search

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -5,13 +5,21 @@ import {
 const useSearch = ({ pathname, pattern }) => {
   const navigate = useNavigate();
   const match = matchPath(pattern, pathname);
-  const search = params => navigate(
-    generatePath(pattern, params)
+  const search = (params = {}) => navigate(
+    generatePath(pattern, Object.fromEntries(
+      Object.entries(params).map(([key, value]) => [
+        key, encodeURIComponent(value)
+      ])
+    ))
   );
+  const params = match
+    ? Object.fromEntries(
+      Object.entries(match.params).map(([key, value]) => [
+        key, decodeURIComponent(value)
+      ])
+    ) : {};
   return {
-    params: match
-      ? match.params
-      : {},
+    params,
     search,
   };
 };
