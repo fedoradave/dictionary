968d7fa912a4dc4655d3b7c99af6d6e70dccd089
David <dwtracey@gmail.com>
2023-06-02 11:37:14 -0400

    danger

--- src/content/Copyright.js
+++ src/content/Copyright.js
@@ -1,6 +1,6 @@
 const Copyright = () => (
   <footer id="copyright">
-    <p>Site &copy;2023, <a target="_blank" href="https://daisydanger.us" rel="noreferrer">Daisy W. Tracey</a></p>
+    <p>Site &copy;2023, <a target="_blank" href="https://daisydanger.us" rel="noreferrer">Daisy W. Danger</a></p>
   </footer>
 );
 
